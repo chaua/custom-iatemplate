73e01581652c445fd8f4a663400c893461a2fc9c
Chaua Queirolo <chaua@users.noreply.github.com>
2021-03-22 18:22:45 -0300

    :sparkles: Atualização do layout e correção das funções matemáticas

--- modelo-pdf.iatemplate/Contents/Resources/res/main.js
+++ modelo-pdf.iatemplate/Contents/Resources/res/main.js
@@ -26,29 +26,29 @@
   })
 
   // Hook up math rendering
-  window.addEventListener( 'DOMContentLoaded', function() {
+  // window.addEventListener( 'DOMContentLoaded', function() {
 
-    function parse( tex ) {
-      try {
-        return TeXZilla.parse( tex )
-      } catch( error ) {
-        return '<math><merror><mtext>' + error.message + '</mtext></merror></math>'
-      }
-    }
+  //   function parse( tex ) {
+  //     try {
+  //       return TeXZilla.parse( tex )
+  //     } catch( error ) {
+  //       return '<math><merror><mtext>' + error.message + '</mtext></merror></math>'
+  //     }
+  //   }
 
-    function render( element ) {
-      element.outerHTML = parse( element.textContent )
-    }
+  //   function render( element ) {
+  //     element.outerHTML = parse( element.textContent )
+  //   }
 
-    function processMath() {
-      var elements = $$( '.math' )
-      for( var i = 0; i < elements.length; i++ ) {
-        render( elements[i] )
-      }
-    }
+  //   function processMath() {
+  //     var elements = $$( '.math' )
+  //     for( var i = 0; i < elements.length; i++ ) {
+  //       render( elements[i] )
+  //     }
+  //   }
 
-    document.body.addEventListener( 'ia-writer-change', processMath )
+  //   document.body.addEventListener( 'ia-writer-change', processMath )
 
-  })
+  // })
 
 })()
